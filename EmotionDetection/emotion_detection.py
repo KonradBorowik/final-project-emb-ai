167537f26cd76a816b1afd9669f9638440a68a2b
Konrad Borowik <konrad1borowik@gmail.com>
2025-08-29 07:13:53 -0400

    code linted with pylint

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,20 +1,26 @@
+''' Module that handles the emotion detection
+'''
 import json
 import requests
 
 
-def emotion_detector(text_to_analyse: str) -> dict:
-    url: str = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+def emotion_detector(text_to_analyze: str) -> dict:
+    ''' Analyze the text and extract the emotion of it
+    '''
+    url: str = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/\
+                NlpService/EmotionPredict'
     headers: dict = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input_data: dict = { "raw_document": { "text": text_to_analyse } }
+    input_data: dict = { "raw_document": { "text": text_to_analyze } }
 
     response: requests.Response = requests.post(
         url=url,
         headers=headers,
-        json=input_data
+        json=input_data,
+        timeout=10
     )
     formatted_response: dict = json.loads(response.text)
     emotions: dict = formatted_response['emotionPredictions'][0]['emotion']
-    
+
     max_emotion_value: float = max(emotions.values())
     for emotion in emotions:
         if emotions[emotion] == max_emotion_value:
---
+++ server.py
@@ -0,0 +1,46 @@
+''' Executing this function initiates the application of emotion
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion detection")
+
+
+@app.route('/emotionDetector')
+def detect_emotion() -> str:
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detector()
+        function. The output returned shows the label and its confidence 
+        score for the provided text.
+    '''
+    text_to_analyze: str = request.args.get('textToAnalyze')
+    response: dict = emotion_detector(text_to_analyze)
+
+    output: str = f"For the given statement, the system response is \
+                    'anger': {response['anger']}, \
+                    'disgust': {response['disgust']}, \
+                    'fear': {response['fear']}, \
+                    'joy': {response['joy']}, \
+                    'sadness': {response['sadness']}. \
+                    The dominant emotion is {response['dominant_emotion']}."
+
+    return output
+
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
